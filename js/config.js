9037d07868b0aaf8af69f32365f6ee09535de537
Jack Dainor <Chambiburgoacarlosdainor@gmail.com>
2026-09-08 17:09:03 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,7 @@
+
+
+// Tu WhatsApp (intermediario) - con código de país 591
+var WHATSAPP_INTERMEDIARIO = "59163614354";
+
+// Tu correo (ya está en el formulario de upload.html)
+// chambiburgoacarlosdainor@gmail.com
